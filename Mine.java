6a630054d1e57afa5aac993fff9cb45a72998d0d
jerrykcode <jerry010201@163.com>
2017-12-19 22:30:04 +0800

    Finish Mine.java

--- Mine.java
+++ Mine.java
@@ -1,29 +1,72 @@
 package mine;
+
 import java.awt.*;
+
 /*
  * Min.java
  * --------
  * This class defines a rectangle indicates a mine 
  */
-public class Mine {
+public class Mine implements MineConstants {
 
 	public Mine(int flag) {
 		// TODO Auto-generated constructor stub
 		this.flag = flag;
 		visible = false;
+		hasSign = false;
 	}
-	
-	public void draw(Graphics g, int x, int y){
-		if (visible){ //If the rectangle is visible
-			if (flag == -1){ //flag == -1 indicates that a land mine insides this rectangle
-				
+
+	/** Returns the flag of this rectangle */
+	public int getFlag() {
+		return flag;
+	}
+
+	/** Returns the visibility of the rectangle */
+	public boolean getVisiblity() {
+		return visible;
+	}
+
+	/** Sets the visibility of the rectangle to be true */
+	public void setVisibility() {
+		visible = true;
+	}
+
+	/** Adds a sign on a rectangle with a visibility equals false */
+	public void addSign() {
+		if (!visible)
+			hasSign = true;
+	}
+
+	public void draw(Graphics g, int x, int y) {
+		if (visible)
+			g.setColor(Color.gray);
+		else
+			g.setColor(Color.green);
+		g.fillRect(x, y, MINE_SIZE, MINE_SIZE);
+		if (visible) { // If the rectangle is visible
+			if (flag == -1) { // flag == -1 indicates that a land mine insides
+								// this rectangle
+				g.setColor(Color.red);
+				g.fillOval(x + MINE_SIZE / 6, y + MINE_SIZE / 6, MINE_SIZE * 2 / 3, MINE_SIZE * 2 / 3);
+			} else if (flag != 0) {
+				g.setColor(Color.cyan);
+				g.drawString(" " + flag, x + MINE_SIZE / 3, y + MINE_SIZE * 2 / 3);
 			}
+		} else if (hasSign) {// Not visible and there is a sign on it
+			// Draw the sign
+			g.setColor(Color.red);
+			int xPoints[] = { x + MINE_SIZE / 3, x + MINE_SIZE / 3, x + MINE_SIZE * 2 / 3 };
+			int yPoints[] = { y + MINE_SIZE / 3, y + MINE_SIZE * 2 / 3, y + MINE_SIZE / 2 };
+			int nPoints = xPoints.length;
+			g.fillPolygon(xPoints, yPoints, nPoints);
 		}
 	}
 
 	/* Private instance variables */
-	private int flag; //-1 indicates that a land mine insides this rectangle
-						//if flag >= 0 && <= 8, flag is the number of land mines around
-						//this rectangle
-	private boolean visible;
+	private int flag; // -1 indicates that a land mine insides this rectangle
+						// if flag >= 0 && <= 8, flag is the number of land
+						// mines around
+						// this rectangle
+	private boolean visible;// true if the rectangle is visible
+	private boolean hasSign;// true if there is a sign on the rectangle
 }
---
+++ MineConstants.java
@@ -0,0 +1,10 @@
+package mine;
+
+public interface MineConstants {
+	
+	/** The size (width and height) of the rectangle indicates a mine */
+	
+	public static final int MINE_SIZE = 30;
+	
+
+}
