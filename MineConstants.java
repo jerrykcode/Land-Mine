d0cdb70a481c20235a2086a04d926cceffc34db1
jerrykcode <jerry010201@163.com>
2017-12-20 12:02:36 +0800

    Add some constants in interface MineConstants

--- MineConstants.java
+++ MineConstants.java
@@ -6,5 +6,13 @@ public interface MineConstants {
 	
 	public static final int MINE_SIZE = 30;
 	
+	/** The number of mine rectangles in one row */
+	
+	public static final int N_MINE_ROW = 19;
+	
+	/** The number of mine rectangles in one column */
+	
+	public static final int N_MINE_COLUMN = 19;
+	
 
 }
